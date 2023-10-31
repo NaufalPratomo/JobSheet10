db81c5cf3f12b59ede91c931571fae000feb16f5
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-10-31 15:26:28 +0700

    modifikasi

--- BioskopWithScanner19.java
+++ BioskopWithScanner19.java
@@ -5,28 +5,58 @@ public class BioskopWithScanner19 {
         Scanner sc = new Scanner(System.in);
 
         int baris, kolom;
-        String nama, next;
+        String nama;
         
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            int menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    do {
+                        System.out.print("Masukkan baris: ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                        if (baris > 0 && baris <= penonton.length && kolom > 0 && kolom <= penonton[0].length) {
+                            if (penonton[baris-1][kolom-1] == null) {
+                                penonton[baris-1][kolom-1] = nama;
+                                break;
+                            } else {
+                                System.out.println("Kursi sudah terisi, silakan pilih kursi lain.");
+                            }
+                        } else {
+                            System.out.println("Kursi tidak tersedia :C");
+                        }
+                    } while (true);
+                    break;
+                case 2:
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            penonton[i][j] = "***";
+                        }
+                    }
+                }
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+                }
+                    break;
+                case 3:
+                    System.out.print("Terimakasih :D");
+                    return;
             }
-
         }
     }
 }
